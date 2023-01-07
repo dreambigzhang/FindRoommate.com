a24ac2ef31b7b7717eb9b038619e194eb2f9eee1
dreambigzhang <alan.yushu.zhang@gmail.com>
2023-01-07 16:23:31 -0700

    like and dislike button now run python script

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/run-like-script')
+def run_like_script():
+    print('Like')
+    #result = 'Like!'
+@app.route('/run-dislike-script')
+def run_dislike_script():
+    print('Dislike')
+
+
 if __name__ == '__main__':
     app.run()
 
