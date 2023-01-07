94ee4400ee389f17bbddb37f934d95fee12a2e0d
dreambigzhang <alan.yushu.zhang@gmail.com>
2023-01-07 15:00:18 -0700

    gradient descent initial commit 5D tensor input 1D tensor output

---
+++ gradientDescent.py
@@ -0,0 +1,30 @@
+import torch
+
+# Define the model
+model = torch.nn.Linear(5, 1)
+
+# Define the loss function and optimizer
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+# Generate some random data
+x = torch.randn(100, 5)
+y = torch.randn(100, 1)
+
+# Training loop
+for i in range(20):
+  # Forward pass
+  y_pred = model(x)
+  loss = loss_fn(y_pred, y)
+
+  # Print gradient and bias
+  print('Iteration:', i)
+  print('Gradient:', model.weight.grad)
+  print('Bias:', model.bias.grad)
+
+  # Backward pass
+  optimizer.zero_grad()
+  loss.backward()
+
+  # Update weights
+  optimizer.step()
