c9033cacae88bbcc8a520ab13e118c9bfd8fd39c
Sherali5543 <shaheerazamkhan@gmail.com>
2023-01-07 16:29:22 -0700

    old

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    name = 'Karen'
+    age = '30'
+    bio = 'I like dogs'
+    location = 'Kentucky'
+    profile_image = 'testprofile.png'
+    return render_template('index.html', name=name, age=age, location=location, bio=bio, pfimg = profile_image)
+
 
 if __name__ == '__main__':
     app.run()
