11eec2dee76ab795b11c99fa0f4ebab647cc38b0
dreambigzhang <alan.yushu.zhang@gmail.com>
2023-01-07 18:45:22 -0700

    added mini-batch dataloader and generator

--- gradientDescent.py
+++ gradientDescent.py
@@ -1,15 +1,35 @@
 import torch
 
+batch_size = 10
+parameter_size = 12
 # Define the model
-model = torch.nn.Linear(12, 1)
+model = torch.nn.Linear(parameter_size, 1)
+
 
 # Define the loss function and optimizer
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
 # Generate some random data
-x = torch.randn(10, 12)
-y = torch.randn(10, 1)
+
+x = torch.tensor([])
+y = torch.tensor([])
+
+i = 0
+while i < batch_size:
+  newInput = torch.randn(1, parameter_size)
+  #print(newInput)
+  '''
+  newResponse = float(input('Response: '))
+  newResponse = torch.tensor([[newResponse]])
+  '''
+  #print(newResponse)
+  newResponse = torch.randn(1, 1)
+  x = torch.cat((x, newInput),0)
+  y = torch.cat((y, newResponse),0)
+  print('Accummulative input:', x, '\nAccummulative response:', y)
+  i+=1
+
 
 # Training loop
 for i in range(10):
@@ -29,3 +49,29 @@ for i in range(10):
   # Update weights
   optimizer.step()
 
+
+newUserParameters = torch.randn(100, 13)
+for i, x in enumerate(newUserParameters):
+  #print(i)
+  rating = torch.dot(model.weight.grad[0], x[:-1])
+  rating = rating.item() + model.bias.grad.item()
+  newUserParameters[i][-1] = rating
+  #print('Rating:',rating)
+  #print(newUserParameters[i])
+
+newUserParameters = sorted(newUserParameters, key = lambda x: x[-1], reverse = True)
+#newUserParameters.tolist()
+for i in range(100):
+  print(newUserParameters[i].tolist()[12])
+  newUserParameters[i] = newUserParameters[i].tolist()[:12]
+newUserParameters = newUserParameters[:10]
+item = newUserParameters[0]
+print('size:', len(item))
+'''
+for item in newUserParameters:
+  print(item)
+  '''
+'''
+newUserParameters = [x in newUserParameters]
+print(newUserParameters)
+'''
