82776856d1973222714a16993197a4f5601a5920
Sherali5543 <shaheerazamkhan@gmail.com>
2023-01-08 11:10:50 -0700

    final changes

--- app.py
+++ app.py
@@ -14,22 +14,94 @@ def main():
 @app.route('/swipe')
 def start():
     global RoomPF
+    #smoke
+    if RoomPF[1] > 0.5:
+        smoke = 'Yes'
+    else: 
+        smoke = 'No'
+    #pet
+    if RoomPF[3] > 0.5:
+        pet = 'do'
+    else:
+        pet = 'don''t'
+    #instrument
+    if RoomPF[4] > 0.8:
+        instrument = 'Guitar'
+    elif RoomPF[4] > 0.6:
+        instrument = 'Violin'
+    elif RoomPF[4] > 0.4:
+        instrument = 'Piano'
+    elif RoomPF[4] > 0.2:
+        instrument = 'Trumpet'
+    else:
+        instrument = 'None'
+    #cleaning
+    if RoomPF[5] > 0.8:
+        clean = 'Daily'
+    elif RoomPF[5] > 0.5:
+        clean = 'Weekly'
+    elif RoomPF[5] > 0.2:
+        clean = 'Biweekly'
+    else:
+        clean = 'I don''t know how to clean'
+    #cook
+    if RoomPF[6] > 0.8:
+        cook = 'I''m basically a commercial chef'
+    elif RoomPF[6] > 0.5:
+        cook = 'I''m better than average'
+    elif RoomPF[6] > 0.2:
+        cook = 'I''m average'
+    else:
+        cook = 'I don''t know how to cook'
+    #friends
+    if RoomPF[7] > 0.7:
+        friends = 'I have freinds over alot'
+    elif RoomPF[7] > 0.4:
+        friends = 'I have friends over occasionaly'
+    else:
+        friends = 'I rarely have friends over'
+    #wake
+    if RoomPF[8] > 0.7:
+        wake = 'I wake up before 8am'
+    elif RoomPF[8] > 0.4:
+        wake = 'I wake up before 10am'
+    else:
+        wake = 'I wake up eventually'
+    #sleep
+    if RoomPF[9] > 0.8:
+        sleep = 'I go to bed eventually'
+    elif RoomPF[9] > 0.5:
+        sleep = 'I go to bed by midnight'
+    elif RoomPF[9] > 0.3:
+        sleep = 'I go to bed by 10pm'
+    else:
+        sleep = 'I''m in bed by 8pm'
+    #decorate
+    if RoomPF[10] > 0.5:
+        decorate = 'I need to decorate everything'
+    else:
+        decorate = 'I don''t care much for decorations'
+    #noise
+    if RoomPF[11] > 0.7:
+        noise = 'I tend to be quite noisy'
+    elif RoomPF[11] > 0.4:
+        noise = 'I don''t tend to make much noise'
+    else:
+        noise = 'I''m as quiet as a mouse'
     profile = interface.request_user_profile_from_backend()
     return render_template('index.html', name=profile.name, 
     age=profile.age, location=profile.location, 
     bio=profile.bio, pfimg=profile.profile_picture,
-    gender=round(RoomPF[0], 4),
-    smoke=round(RoomPF[1], 4),
-    candle=round(RoomPF[2], 4),
-    pet=round(RoomPF[3], 4),
-    instrument=round(RoomPF[4], 4),
-    clean=round(RoomPF[5], 4),
-    cook=round(RoomPF[6], 4),
-    friends=round(RoomPF[7], 4),
-    wake=round(RoomPF[8], 4),
-    sleep=round(RoomPF[9], 4),
-    decorate=round(RoomPF[10], 4),
-    noise=round(RoomPF[11], 4))
+    smoke=smoke,
+    pet=pet,
+    instrument=instrument,
+    clean=clean,
+    cook=cook,
+    friends=friends,
+    wake=wake,
+    sleep=sleep,
+    decorate=decorate,
+    noise=noise)
     
 @app.route('/refresh')
 def refresh():
