10ba58d44ef3c88e8b4b7b39bd0e9f1cc3917f1c
Sherali5543 <shaheerazamkhan@gmail.com>
2023-01-07 17:03:41 -0700

    new changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+name = 'Karen'
+age = '30'
+bio = 'I like dogs'
+location = 'Kentucky'
+profile_image = './static/testprofile.png'
 @app.route('/')
 def main():
-    name = 'Karen'
-    age = '30'
-    bio = 'I like dogs'
-    location = 'Kentucky'
-    profile_image = 'testprofile.png'
     return render_template('index.html', name=name, age=age, location=location, bio=bio, pfimg = profile_image)
 
 
@@ -20,6 +20,9 @@ def run_like_script():
 def run_dislike_script():
     print('Dislike')
 
+@app.route('/liked')
+def new():
+    return render_template('liked.html')
 
 if __name__ == '__main__':
     app.run()
